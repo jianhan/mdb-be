6c64ffadfb250e979e55a169a8aa67234101a082
Jian Han <jianhan@mail.com>
2019-12-23 21:31:33 +1000

    Updated package json to have 3 build envs and docker file.

--- gql/webpack.development.js
+++ gql/webpack.development.js
@@ -8,13 +8,9 @@ const common = require('./webpack.common.js');
 
 module.exports = merge.smart(common, {
   devtool: 'inline-source-map',
-  entry: ['webpack/hot/poll?1000', path.join(__dirname, 'src/main.ts')],
-  externals: [
-    nodeExternals({
-      whitelist: ['webpack/hot/poll?1000']
-    })
-  ],
+  entry: [path.join(__dirname, 'src/main.ts')],
+  externals: [nodeExternals({})],
   mode: 'development',
   plugins: [new CleanWebpackPlugin(), new webpack.HotModuleReplacementPlugin()],
-  watch: true
+  watch: false
 });

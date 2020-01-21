bbd0e3dc7c72fcb891c6f3da82779dc122d2ecf0
Jian Han <jianhan@mail.com>
2020-01-21 18:52:38 +1000

    Finished unit test for users lookup

--- tw/src/users/lookup.unit.test.ts
+++ tw/src/users/lookup.unit.test.ts
@@ -1,10 +1,16 @@
-import {constructParams} from "./lookup";
+import R from "ramda";
+import {constructParams, getAPIParams} from "./lookup";
 import UsersLookupParameters from "./UsersLookupParameters";
+
+let params: UsersLookupParameters;
+
+beforeEach(() => {
+    params = new UsersLookupParameters(["test   ", "test1    ", "test", "test1"], [1, 2, 3, 1, 1, 2, 2], true, true);
+});
 
 describe("function convertScreenNames", () => {
 
     it("should convert all properties", () => {
-        const params = new UsersLookupParameters(["test   ", "test1    ", "test", "test1"], [1, 2, 3, 1, 1, 2, 2], true, true);
         // @ts-ignore
         const apiParams = constructParams(params)({});
         expect(apiParams).toEqual({
@@ -13,6 +19,31 @@ describe("function convertScreenNames", () => {
             user_id: "1,2,3",
             screen_name: "test,test1",
         });
+    });
+
+});
+
+describe("function getAPIParams", () => {
+
+    it("should return valid api params with valid input", () => {
+        const result = getAPIParams(params);
+        expect(result._tag).toBe("Right");
+    });
+
+    it("should fail when screenNames exceed max size", () => {
+        params.screenNames = R.range(1, 102).map(x => "test name " + x);
+        expect(getAPIParams(params)._tag).toBe("Left");
+
+        params.screenNames = R.range(1, 101).map(x => "test name " + x);
+        expect(getAPIParams(params)._tag).toBe("Right");
+    });
+
+    it("should fail when userIds exceed max size", () => {
+        params.userIds = R.range(1, 102);
+        expect(getAPIParams(params)._tag).toBe("Left");
+
+        params.userIds = R.range(1, 101);
+        expect(getAPIParams(params)._tag).toBe("Right");
     });
 
 });

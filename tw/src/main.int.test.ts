8292cf0508252e0760e5ad8e619bd556e1addb06
Jian Han <jianhan@mail.com>
2020-02-08 11:36:11 +1000

    Added integration tests for parameters in main.ts

--- tw/src/main.int.test.ts
+++ tw/src/main.int.test.ts
@@ -1,24 +1,10 @@
-import {Map} from "immutable";
-import {Logger} from "winston";
-import {Environment, LogLevel} from "../../lib/src/constants";
-// @ts-ignore
-import {getEnvs} from "../../lib/src/envs";
-// @ts-ignore
-import {createLogger} from "../../lib/src/logger";
-import {Envs} from "./envs";
+import _ from "lodash";
 import {handler} from "./main";
 
 jest.setTimeout(12000);
 
-let envs: Map<string, string | Environment | undefined>;
-let logger: Logger;
-
-beforeEach(async () => {
-    envs = await getEnvs(process.env, Envs);
-    logger = createLogger(envs.get("NODE_ENV") as Environment, envs.get("SERVICE_NAME") as string, LogLevel.DEBUG);
-});
-
 describe("main function", () => {
+
     it("should invoke lambdaFunc and process the request", async () => {
         // @ts-ignore
         const r = await handler({
@@ -26,6 +12,34 @@ describe("main function", () => {
             // @ts-ignore
             body: {screenNames: ["chenqiushi404"]},
         });
-        logger.info(r, "******");
+        expect(_.has(r, "statusCode")).toBe(true);
+        expect(_.get(r, "statusCode")).toBe(200);
+        expect(_.has(r, "body")).toBe(true);
+        expect(_.get(r, "body")).toBe("function completed");
+        expect(_.has(r, "response.ETag")).toBe(true);
+        expect(_.get(r, "response.ETag")).not.toBe("");
     });
-})
+
+    it("should invoke lambdaFunc and return invalid response with invalid body parameter", async () => {
+        // @ts-ignore
+        const r = await handler({
+            path: "./users/index",
+            // @ts-ignore
+            body: "test",
+        });
+        expect(_.has(r, "statusCode")).toBe(true);
+        expect(_.get(r, "statusCode")).toBe(400);
+    });
+
+    it("should not invoke lambdaFunc when path parameter is not valid", async () => {
+        // @ts-ignore
+        const r = await handler({
+            path: "./users/test",
+            // @ts-ignore
+            body: "test",
+        });
+        expect(_.has(r, "statusCode")).toBe(true);
+        expect(_.get(r, "statusCode")).toBe(400);
+    });
+
+});
